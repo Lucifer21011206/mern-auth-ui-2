20bcf37e1f5a172bad0bd0ad77a1729018c718a8
Lucifer21011206 <104339478+Lucifer21011206@users.noreply.github.com>
2025-01-31 12:49:45 +0530

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,32 @@
-import { Grid, Card, CardContent } from "@mui/material";
+import { Grid, Card, CardContent , Box} from "@mui/material";
 import LeftImageSection from "../components/LeftImageSection";
 import LoginForm from "../components/LoginForm";
 
 const Login = () => {
-  return (
+  return <>
+  <Box
+        sx={{
+          position: "absolute",
+          top: 25,
+          left: 35,
+          zIndex: 1000, // Ensure the logo is above other elements
+        }}
+      >
+        <img
+          src="../assets/Logo.png" // Path to your logo image
+          alt="Logo"
+          style={{
+            width: "100px", // Adjust the size as needed
+            height: "100px",
+            borderRadius:"50%" // Maintain aspect ratio
+          }}
+        />
+      </Box>
     <Grid
       container
       justifyContent="center"
       alignItems="center"
-      sx={{ minHeight: "100vh", backgroundColor: "#1e1e2f", p: 2 ,background: "linear-gradient(135deg, #00ff99, #ff0080, #00eaff)"}}
+      sx={{ minHeight: "100vh", backgroundColor: "#1e1e2f", p: 2 ,background: "linear-gradient(135deg,rgb(248, 99, 44), #FFB347, #FF7F50)"}}
     >
       <Card elevation={24} sx={{ maxWidth: 900 , width: "100%", borderRadius: 5, overflow: "hidden" }}>
         <Grid container>
@@ -26,7 +44,7 @@ const Login = () => {
         </Grid>
       </Card>
     </Grid>
-  );
+  </>;
 };
 
 export default Login;
